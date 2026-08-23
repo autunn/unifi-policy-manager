e4f2bcdab171129d4cc70d5e4437143b0723d19a
Autumn <44563868+autunn@users.noreply.github.com>
2026-08-23 21:30:06 +0800

    Restore WPF input event namespace

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Text.Json;
 using Microsoft.Win32;
